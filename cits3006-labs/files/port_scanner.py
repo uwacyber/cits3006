937f8e269988ab218686b7c75d187166a9f862ae
uwacyber <98138371+uwacyber@users.noreply.github.com>
2022-07-18 22:47:09 +0800

    Create port_scanner.py

---
+++ cits3006-labs/files/port_scanner.py
@@ -0,0 +1,31 @@
+from socket import *
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', dest='target', help='Target IP Address/Adresses')
+    options = parser.parse_args()
+
+    #Check for errors i.e if the user does not specify the target IP Address
+    #Quit the program if the argument is missing
+    #While quitting also display an error message
+    if not options.target:
+        #Code to handle if interface is not specified
+        parser.error("[-] Please specify an IP Address or Addresses, use --help for more info.")
+    return options
+  
+def main():
+  if __name__ == '__main__':
+     target = getargs().target
+     t_IP = gethostbyname(target)
+     print ('Starting scan on host: ', t_IP)
+
+     for i in range(1, 1024):
+        s = socket(AF_INET, SOCK_STREAM)
+
+        conn = s.connect_ex((t_IP, i))
+        if(conn == 0) :
+           print ('Port %d: OPEN' % (i,))
+        s.close()
+  
+main()
