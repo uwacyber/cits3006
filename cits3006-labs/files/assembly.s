66e88749531aba0930c6b5f95566af828ec5b935
uwacyber <98138371+uwacyber@users.noreply.github.com>
2022-07-27 15:59:07 +0800

    Update assembly.s

--- cits3006-labs/files/assembly.s
+++ cits3006-labs/files/assembly.s
@@ -1,34 +1,21 @@
-	.arch armv8-a
-	.file	"file.c"
-	.text
-	.align	2
-	.global	factorial
-	.type	factorial, %function
+    .file    "factorial.c"
+    .text
+.globl factorial
+    .type    factorial, @function
 factorial:
-.LFB0:
-	.cfi_startproc
-	str	x19, [sp, -32]!
-	.cfi_def_cfa_offset 32
-	.cfi_offset 19, -32
-	str	w0, [sp, 28]
-	mov	w19, 1
-	b	.L2
-.L3:
-	ldr	w0, [sp, 28]
-	sub	w1, w0, #1
-	str	w1, [sp, 28]
-	mul	w19, w19, w0
+    testl    %eax, %eax
+    jne    .L2
+    movl    $1, %edx
+    jmp    .L4
 .L2:
-	ldr	w0, [sp, 28]
-	cmp	w0, 0
-	bne	.L3
-	mov	w0, w19
-	ldr	x19, [sp], 32
-	.cfi_restore 19
-	.cfi_def_cfa_offset 0
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	factorial, .-factorial
-	.ident	"GCC: (Debian 11.3.0-1) 11.3.0"
-	.section	.note.GNU-stack,"",@progbits
+    movl    $1, %edx
+.L5:
+    imull    %eax, %edx
+    decl    %eax
+    jne    .L5
+.L4:
+    movl    %edx, %eax
+    ret
+    .size    factorial, .-factorial
+    .ident    "GCC: (GNU) 4.1.2 20060715 (prerelease) (Debian 4.1.1-9)"
+    .section    .note.GNU-stack,"",@progbits
